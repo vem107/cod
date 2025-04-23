a3118a64f7a0d1e63683ed9b09081b233ead29c0
vem107 <demos62016737@gmail.com>
2025-04-23 21:57:35 +0530

    Delete Assignment-7-WordCount.py

--- Assignment-7-WordCount.py
+++
@@ -1,6 +0,0 @@
-from pyspark import SparkContext
-sc = SparkContext()
-text_rdd = sc.textFile("random.txt")
-word_count = text_rdd.flatMap(lambda x:x.split(" ")).map(lambda x:(x,1)).reduceByKey(lambda x,y:x+y).collect()
-print(word_count)
-sc.stop()
